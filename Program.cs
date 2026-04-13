596ce241f0b5be65042de8fdfe51fe801ac59a99
Nitu <nitu.kumari@sisindia.com>
2026-04-13 16:58:30 +0530

    added radzen service comment

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace MySISWeb
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
-
+           //addv radzen dialog service
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
