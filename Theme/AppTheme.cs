1ca0af6a07306610abd0f4bf0a8eaa1649007566
Nitu <nitu.kumari@sisindia.com>
2026-04-14 12:38:10 +0530

    Theme added

---
+++ Theme/AppTheme.cs
@@ -0,0 +1,35 @@
+﻿using MudBlazor;
+
+public static class AppTheme
+{
+    public static MudTheme MyTheme = new MudTheme()
+    {
+        PaletteLight = new PaletteLight()
+        {
+            Primary = "#CA252B",
+            Secondary = "#94A3B8",
+            Success = "#22C55E",
+            Warning = "#EAB308",
+
+            Background = "#F8FAFC",
+            Surface = "#FFFFFF",
+
+            AppbarBackground = "#CA252B",
+            AppbarText = "#FFFFFF",
+
+            DrawerBackground = "#FFFFFF",
+            DrawerText = "#1E293B",
+
+            TextPrimary = "#1E293B",
+            TextSecondary = "#64748B",
+        },
+
+        Typography = new Typography()
+        {
+            Default = new DefaultTypography()
+            {
+                FontFamily = new[] { "Inter", "sans-serif" }
+            }
+        }
+    };
+}
